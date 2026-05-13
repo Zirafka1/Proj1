dd0081545639d94f71c1aeaa04698379d2344352
Kronixlegends <kronixisgood@gmail.com>
2026-05-13 10:08:10 +0200

    Pridani popisu kodu
    ...

--- Hra/Movement.py
+++ Hra/Movement.py
@@ -7,16 +7,22 @@ bullet_speed = 8
 #HLAVNÍ HERNÍ SMYČKA
 
 while True:
-    screen.fill(BLACK)
-
+    screen.fill(BLACK)   
+    
+    #zpracovani vsech udalosti
     for event in pygame.event.get():
+
+        #pokud se hra zavre
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            pygame.quit() #ukonci
+            sys.exit() #zavre okno
+            
+        #pokud je zmacknuta klavesa
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                bullet = pygame.Rect(ship.centerx - 3, ship.y, 6, 15)
-                bullets.append(bullet)
+                bullet = pygame.Rect(ship.centerx - 3, ship.y, 6, 15) #vytvori novou strelu na obrazovce
+                bullets.append(bullet) #zaradi strelu do seznamu strel
+                
     #POHYB LODIČKY
     keys = pygame.key.get_pressed()
 
@@ -27,10 +33,11 @@ while True:
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         ship.x += ship_speed
 
-
+    #zabrani vyjeti lodi na leve obrazovce
     if ship.x < 0:
         ship.x = 0
 
+    #zabrani vyjeti lodi na prave obrazovce
     if ship.x > WIDTH - ship.width:
         ship.x = WIDTH - ship.width
 
